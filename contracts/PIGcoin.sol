38f7f91d27798b88d781ea5bd2989a1a615b0897
GClau <clnr@eppr.link>
2022-02-12 03:12:42 +0000

    Listos para crear nuestra primera moneda virtual

---
+++ contracts/PIGcoin.sol
@@ -0,0 +1,15 @@
+// Definir la version de Sol.
+pragma solidity ^0.6.2
+// Importar contratos
+
+// Crear contrato propio
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.4/contracts/token/ERC20/ERC20.sol"
+
+contract PIGcoin is ERC20 {
+
+    constructor() public ERC20("PIGcoin", "PICO") {
+        // MINT 
+        _mint(msg.sender, 1000000 * ( 10 ** uint256( decimals() ) ) );
+    }
+}
